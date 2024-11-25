6d06165c481088a14839d88b49b9e5b3e3e8077d
Skye <swskye17@gmail.com>
2024-11-26 01:38:21 +0300

    "Security improvement"
    Signed-off-by: Skye <swskye17@gmail.com>

--- src/renderer/index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const titleElement = document.getElementById('title');
-    titleElement.innerText = 'Welcome to My Electron App';
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    aichat: './src/common/js/aichat.js'
+    aichat: './src/renderer/js/aichat.js'
   },
   output: {
     filename: '_[name].js', // This will generate files named after the entry points
-    path: path.resolve(__dirname, './src/common/js'),
+    path: path.resolve(__dirname, './src/renderer/js'),
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser', // Provide process polyfill
+    }),
+  ],
+
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify'),
+      vm: require.resolve('vm-browserify'),
+      buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+      fs: require.resolve('fs')
+    }
   },
   mode: 'development',
 };
