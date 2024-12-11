b879cc178f61005db24e5662c607af4e43e2e8c3
Skye <swskye17@gmail.com>
2024-12-12 02:14:38 +0300

    Rolling out new features
    full conversation copy
    conversation export->pdf/png/docx
    
    Signed-off-by: Skye <swskye17@gmail.com>

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,10 +1,41 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, shell } = require('electron');
 const crypto = require('crypto');
 const { Buffer } = require('buffer');
+const fs = require('fs');
+const path = require('path');
 
 contextBridge.exposeInMainWorld('electron', {
     getEnv: () => ipcRenderer.invoke('get-env'),
     getBufferFromIV: (iv) => ipcRenderer.invoke('get-buffer-from-iv', iv),
     scryptSync: (password, salt, keylen) => crypto.scryptSync(password, salt, keylen),
-    Buffer
+    Buffer,
+    getDownloadsPath: () => {
+        const homedir = require('os').homedir();
+        const downloadsPath = path.join(homedir, 'Downloads');
+        return downloadsPath;
+    },
+    saveAndOpenImage: (downloadsPath, dataUrl) => {
+        fetch(dataUrl)
+        .then(res => res.blob())
+        .then(blob => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const buffer = Buffer.from(reader.result);
+                const outputPath = path.join(downloadsPath, 'output.jpg');
+
+                fs.writeFile(outputPath, buffer, (err) => {
+                    if (err) {
+                        console.error('Error saving image:', err);
+                    } else {
+                        shell.openPath(outputPath);
+                    }
+                });
+            };
+            reader.readAsArrayBuffer(blob);
+        })
+        .catch((error) => {
+            console.error('Error creating blob:', error);
+        });
+    },
 });
+
