8fcaa2af13f6e58f494c9cb85a79e319522de6ea
Skye <swskye17@gmail.com>
2025-01-25 10:58:36 +0300

    updated storage location to .quickai home folder
    Signed-off-by: Skye <swskye17@gmail.com>

--- src/renderer/js/chatStore.js
+++ src/renderer/js/chatStore.js
@@ -1,4 +1,4 @@
-const storagePath = window.electron.joinPath(window.electron.home_dir(), '.quickai.store');
+const storagePath = window.electron.joinPath(window.electron.home_dir(), '.quickai/.quickai.store');
 const chatArea = document.getElementById('chatArea');
 const conversationsPanel = document.getElementById('conversations');
 escapeHTML = window.escapeHTML;
