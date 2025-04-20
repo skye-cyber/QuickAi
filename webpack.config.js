52f7e545635487640ad0f6244fd7c9c73cc6ccf7
Skye <swskye17@gmail.com>
2025-04-21 01:25:58 +0300

    Repack scripts
    Signed-off-by: Skye <swskye17@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 const entries = {
-    //hfaichat: './src/renderer/js/hfaichat.js',
-    // utility: './src/renderer/js/exportUtilities.js',
+    hfaichat: './src/renderer/js/hfaichat.js',
+    utility: './src/renderer/js/exportUtilities.js',
     MistralChatsAdmin: './src/renderer/js/MistralChatsAdmin.js',
     chatUtils: './src/renderer/js/chatUtils.js',
-    // hfaudio: './src/renderer/js/hfaudio.js',
+    hfaudio: './src/renderer/js/hfaudio.js',
     dotDraw: './src/renderer/js/diagraming/dotDraw.js',
-    // preference: './src/renderer/js/preference.js',
-    // keyshortcuts: './src/renderer/js/keyshortcuts.js',
-    // fileHandler: './src/renderer/js/fileHandler.js'
+    preference: './src/renderer/js/preference.js',
+    keyshortcuts: './src/renderer/js/keyshortcuts.js',
+    fileHandler: './src/renderer/js/fileHandler.js'
 };
 
 module.exports = Object.entries(entries).map(([name, entryPath]) => {
